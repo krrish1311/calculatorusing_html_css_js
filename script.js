00f3ae14c202e435c1c8469be6cd8ec81447e1ed
Krishnakant Digole <krrishdigole@gmail.com>
2023-01-11 11:24:32 +0530

    The extra scientific features are added

--- script.js
+++ script.js
@@ -9,6 +9,44 @@ for (i of btnVal) {
     console.log(screen.value);
   });
 }
+function percentage() {
+  screen.value = screen.value / 100;
+}
+function sin() {
+  screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+  screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+  screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+  screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+  screen.value = Math.log(screen.value);
+}
+
+function pi() {
+  screen.value += 3.14159265359;
+}
+
+function e() {
+  screen.value += 2.71828182846;
+}
+function divby1() {
+  screen.value = 1 / screen.value;
+}
+
 function backspc() {
   screen.value = screen.value.substr(0, screen.value.length - 1);
 }
